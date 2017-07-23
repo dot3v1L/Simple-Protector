fc61f7cf2a3d9516d71ea978aa4d94a9c8f79034
dot3v1L <cryptonly4you@gmail.com>
2017-07-23 13:50:18 +0200

    master

---
+++ Simple Protect/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Simple_Protect
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fPath = new System.Windows.Forms.TextBox();
+            this.fOpen = new System.Windows.Forms.Button();
+            this.fSave = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // fPath
+            // 
+            this.fPath.Location = new System.Drawing.Point(12, 12);
+            this.fPath.Name = "fPath";
+            this.fPath.Size = new System.Drawing.Size(167, 20);
+            this.fPath.TabIndex = 0;
+            // 
+            // fOpen
+            // 
+            this.fOpen.Location = new System.Drawing.Point(197, 10);
+            this.fOpen.Name = "fOpen";
+            this.fOpen.Size = new System.Drawing.Size(75, 23);
+            this.fOpen.TabIndex = 1;
+            this.fOpen.Text = "File";
+            this.fOpen.UseVisualStyleBackColor = true;
+            this.fOpen.Click += new System.EventHandler(this.fOpen_Click);
+            // 
+            // fSave
+            // 
+            this.fSave.Location = new System.Drawing.Point(12, 70);
+            this.fSave.Name = "fSave";
+            this.fSave.Size = new System.Drawing.Size(260, 23);
+            this.fSave.TabIndex = 2;
+            this.fSave.Text = "Protect";
+            this.fSave.UseVisualStyleBackColor = true;
+            this.fSave.Click += new System.EventHandler(this.fSave_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Rename types";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(107, 47);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(109, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Rename methods";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 104);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.fSave);
+            this.Controls.Add(this.fOpen);
+            this.Controls.Add(this.fPath);
+            this.Name = "fMain";
+            this.Text = "Simple Protect";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fPath;
+        private System.Windows.Forms.Button fOpen;
+        private System.Windows.Forms.Button fSave;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Simple Protect/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Mono.Cecil;
+
+namespace Simple_Protect
+{
+    public partial class fMain : Form
+    {
+        private AssemblyDefinition asm;
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void fOpen_Click(object sender, EventArgs e)
+        {
+            using (var open = new OpenFileDialog())
+            {
+                open.Filter = ".exe|*.exe";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    fPath.Text = open.FileName;
+                    asm = AssemblyDefinition.ReadAssembly(fPath.Text);
+                }
+            }
+        }
+
+        private void RenameType(AssemblyDefinition assembly)
+        {
+            TypeDefinition mytype = null;
+            foreach (TypeDefinition t in assembly.MainModule.Types)
+            {
+                t.Name = RandomEng(12);
+            }
+        }
+
+        private void RenameMethod(AssemblyDefinition assembly)
+        {
+            MethodDefinition mymethod = null;
+            TypeDefinition mytype = null;
+            foreach (TypeDefinition t in assembly.MainModule.Types)
+            {
+                foreach (MethodDefinition m in t.Methods) // t est un Type
+                {
+                    if (t.Name != "<Module>")
+                    {
+                        m.Name = RandomEng(12);
+                    }
+                }
+            }
+        }
+
+        private readonly Random rnd = new Random();
+        private const string mix = "դгжфֆйবբтзհֆխնйռցтդհўমցсрфўзтгйфবсবтўізռնжтխссհжз৯зৰжжгռз৯сзռдсд৯ցচցтітբзуռհіৰնৰтհрխіমуарпռзджնդ৯ৰпբпфуֆйпբтցўդব৯৯ռհшমৰֆаі";
+        private const string eng = "ABCDIFGHEJKLOMPNRSTQWZadsadioaigysdLOMPNshdnuwqeuyigysdLOMPNRSTQWZadsa";
+
+        public string RandomMix(int size)
+        {
+            char[] buffer = new char[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                buffer[i] = mix[rnd.Next(mix.Length)];
+            }
+            return new string(buffer);
+        }
+
+        public string RandomEng(int size)
+        {
+            char[] buffer = new char[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                buffer[i] = eng[rnd.Next(eng.Length)];
+            }
+            return new string(buffer);
+        }
+
+        private void fSave_Click(object sender, EventArgs e)
+        {
+            using (var save = new SaveFileDialog())
+            {
+                save.Filter = ".exe|*.exe";
+                save.FileName = String.Format("{0}_obf", Path.GetFileNameWithoutExtension(fPath.Text));
+
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    if (checkBox1.Checked)
+                        RenameType(asm);
+                    if (checkBox2.Checked)
+                        RenameMethod(asm);
+                    
+                    asm.Write(save.FileName);
+
+                    MessageBox.Show("Done ;)", "Ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+    }
+}
